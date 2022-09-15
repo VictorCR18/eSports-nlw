4299a8ee868c165cc488972d80d11adec6155976
VictorCR18 <Cavalcantevictor2003@gmail.com>
2022-09-14 23:35:21 -0300

    Versao 1

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json([
+        { id: 1, name: 'Anuncio 1' }
+    ]);
+});
+
+app.listen(6969, () => {
+    console.log("vitu ta on")
+});
