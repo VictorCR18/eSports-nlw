8680092737f5b4e83462f181c295ec96b80846c4
VictorCR18 <Cavalcantevictor2003@gmail.com>
2022-09-15 22:46:04 -0300

    Versao 3

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,32 @@ import express from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/game', (request, response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gamesId = request.params.id;
+
     return response.json([
-        { id: 1, name: 'Anuncio 1' }
+        { id: 1, name: 'Anuncio 1' },
+        { id: 2, name: 'Anuncio 2' },
+        { id: 3, name: 'Anuncio 3' },
+        { id: 4, name: 'Anuncio 4' },
+        { id: 5, name: 'Anuncio 5' },
     ]);
 });
 
-app.listen(6969, () => {
-    console.log("vitu ta on")
+app.get('/ads/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+
+    return response.json([]);
+});
+
+app.listen(3000, () => {
+    console.log("Running...");
 });
